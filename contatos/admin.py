0faf46af51d6e6ff03f9170b44adb9c588c03ea5
Naptoss <tomgabinio@gmail.com>
2022-10-09 13:10:07 -0300

    .

--- contatos/admin.py
+++ contatos/admin.py
@@ -6,10 +6,14 @@ from .models import Categoria, Contato
 
 class ContatoAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome', 'sobrenome', 'data_criacao', 'categoria')
+    # mostra os valores
 
     list_display_links = ('id', 'nome', 'sobrenome')
+    # voce consegue clicar nesses valores e ser levado para o "perfil" dessa pessoa
 
     list_filter = ('nome', 'sobrenome')
+
+    # consegue filtrar pelo nome e sobrenome
 
 
 admin.site.register(Categoria)
