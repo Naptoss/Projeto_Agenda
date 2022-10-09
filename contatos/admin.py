f4cef58d4dbf9a77a0ab4772ad3d0ba28a1476a4
Naptoss <tomgabinio@gmail.com>
2022-10-09 13:10:00 -0300

    .

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Categoria, Contato
 
 # Register your models here.
+
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'data_criacao', 'categoria')
+
+    list_display_links = ('id', 'nome', 'sobrenome')
+
+    list_filter = ('nome', 'sobrenome')
+
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
