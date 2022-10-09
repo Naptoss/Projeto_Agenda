9038acdb90b50899060f99da1ec4d602a57057b9
Naptoss <tomgabinio@gmail.com>
2022-10-09 12:07:39 -0300

    .

--- contatos/models.py
+++ contatos/models.py
@@ -1,15 +1,20 @@
 from email.policy import default
 from django.db import models
 from django.utils import timezone
+from unittest.util import _MAX_LENGTH
 
 # Create your models here.
 
 
-class Contato(models.Model):
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
 
+
+class Contato(models.Model):
     nome = models.CharField(max_length=255)
     sobrenome = models.CharField(max_length=255, blank=True)
-    telefone = models.CharField(max_Length=255)
-    email = models.CharField(max_Length=255, blank=True)
-    data_criacap = models.DateField(default=timezone.now)
+    telefone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
+    data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
