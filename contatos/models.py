2a74298607d9d072ee788300f180e2690e17af77
Naptoss <tomgabinio@gmail.com>
2022-10-09 11:56:05 -0300

    adicionado a clase Contato e algumas funcoes

--- contatos/models.py
+++ contatos/models.py
@@ -1,3 +1,15 @@
+from email.policy import default
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Contato(models.Model):
+
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255, blank=True)
+    telefone = models.CharField(max_Length=255)
+    email = models.CharField(max_Length=255, blank=True)
+    data_criacap = models.DateField(default=timezone.now)
+    descricao = models.TextField(blank=True)
