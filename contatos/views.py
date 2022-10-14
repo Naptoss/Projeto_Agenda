c3cc949be293c69db8a58c1e0498f2add36b8914
Naptoss <tomgabinio@gmail.com>
2022-10-14 18:09:42 -0300

    .

--- contatos/views.py
+++ contatos/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Contato
+from django.core.paginator import Paginator
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def index(request):
     contatos = Contato.objects.all()
+    paginator = Paginator(contatos, 2)
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
     return render(request, 'contatos/index.html', {
         'contatos': contatos
 
